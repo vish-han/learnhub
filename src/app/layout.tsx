4b0039878d4752a0c7aa43b560d4533a9cbfcd6d
vish-han <vshlchauhab@gmail.com>
2025-08-19 13:47:11 +0530

    metadata updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,29 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "GradifyNow",
-  description: "This is the Webiste for Gradify Now, A platform for all the aspiring doctrate and researchers.",
-  
+  description: "This is the Website for Gradify Now, A platform for all the aspiring doctorate and researchers.",
+  openGraph: {
+    url: "https://learnhub-5lpp.vercel.app",
+    type: "website",
+    title: "GradifyNow",
+    description: "This is the Website for Gradify Now, A platform for all the aspiring doctorate and researchers.",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/images/72d8a9c4-6334-4ae1-9d62-db334ff18601.jpg?token=ERgobjhDUk2bxbbmJxeI09ni9VfSjzK8OtYWCNv3r5g&height=1049&width=1200&expires=33291591311",
+        width: 1200,
+        height: 1049,
+        alt: "GradifyNow - Platform for aspiring doctorate and researchers",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@gradifynow", // Add your Twitter handle if you have one
+    title: "GradifyNow",
+    description: "This is the Website for Gradify Now, A platform for all the aspiring doctorate and researchers.",
+    images: ["https://opengraph.b-cdn.net/production/images/72d8a9c4-6334-4ae1-9d62-db334ff18601.jpg?token=ERgobjhDUk2bxbbmJxeI09ni9VfSjzK8OtYWCNv3r5g&height=1049&width=1200&expires=33291591311"],
+  },
+  metadataBase: new URL("https://learnhub-5lpp.vercel.app"),
 };
 
 export default function RootLayout({
@@ -23,9 +44,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} font-poppins antialiased`}
-      >
+      <body className={`${poppins.variable} font-poppins antialiased`}>
         <Header/>
         {children}
         <Footer/>
